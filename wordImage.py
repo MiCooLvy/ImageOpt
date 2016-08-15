c1b78b49ce192e1bc2b1a6bc8a340632be624a49
MiCooLvy <canceman88@gmail.com>
2016-08-15 15:30:14 +0800

    开启一个线程处理图像，提高灰度图像生成速度

--- wordImage.py
+++ wordImage.py
@@ -30,45 +30,31 @@ def avgthread(image, w, startH, endH, binImg):
             binImg[i, j] = avgPx
 
 
-def binImage(image):
-    imgW = image.shape[0]
-    imgH = image.shape[1]
-    binImg = np.zeros([imgW, imgH], np.uint8)
-    for h in range(imgH):
-        for w in range(imgW):
-            avgPx = average(image[w, h, 0], image[w, h, 1], image[w, h, 2])
-            binImg[w, h] = avgPx
-    return binImg
-
 def main():
     p0 = 'E:/dev/opencv/pyCV/dog.jpg'
     p1 = 'E:/dev/opencv/pyCV/girl01.jpg'
     p2 = 'E:/dev/opencv/pyCV/blank.jpg'
     p3 = 'E:/dev/opencv/pyCV/g02.jpg'
 
-    img = cv2.imread(p0)
+    img = cv2.imread(p3)
     cv2.namedWindow("Image")
     cv2.imshow("Image", img)
-    b, g, r = cv2.split(img)
 
-    # imgH = img.shape[0]
-    # imgW = img.shape[1]
-    # binImg = np.zeros([imgH, imgW], np.uint8)
-    #
-    # t = threading.Thread(target=avgthread, args=(img, imgW, 0, imgH / 2, binImg))
-    # t1 = threading.Thread(target=avgthread, args=(img, imgW, imgH / 2, imgH, binImg))
-    #
-    # t.start()
-    # t1.start()
-    #
-    # t.join()
-    # t1.join()
+    imgH = img.shape[0]
+    imgW = img.shape[1]
+    binImg = np.zeros([imgH, imgW], np.uint8)
 
+    avgthread(img, imgW, 0, imgH / 2, binImg)
 
-    cv2.imshow("binImage", binImage(img))
+    t = threading.Thread(target=avgthread, args=(img, imgW, imgH / 2, imgH, binImg))
+    t.start()
+    t.join()
 
-#    cv2.waitKey(0)
+    cv2.imshow("binImage", binImg)
+
+    cv2.waitKey(0)
     cv2.destroyAllWindows()
+
 
 if __name__ == '__main__':
     main()
